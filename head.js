07c1b0b1f9c500b236d691adf2e45d2a49b12622
Tara MacKinnon <taramackinnon4@gmail.com>
2023-03-07 01:45:50 +0000

    fixed format error

--- head.js
+++ head.js
@@ -19,4 +19,4 @@ const head = function(array) {
 //Test the head function:
 assertEqual(head([5, 6, 7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), ); 
+assertEqual(head([]),);
