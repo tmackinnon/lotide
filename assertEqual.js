ffa9e5efa3f6813681cb17d972929aba1032123e
Tara MacKinnon <taramackinnon4@gmail.com>
2023-03-07 01:10:25 +0000

    added comments

--- assertEqual.js
+++ assertEqual.js
@@ -1,11 +1,12 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
-  //if two parameters don't match
+  //if two inputs don't match
   if (actual !== expected) {
     console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
     return;
   }
 
+  //if two inputs match
   console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
 
 };
