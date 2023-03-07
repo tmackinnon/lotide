646ba9ef0b3a069fbe08c7d469139e949e39ff74
Tara MacKinnon <taramackinnon4@gmail.com>
2023-03-07 01:41:52 +0000

    created head function to return the first item of an array

---
+++ head.js
@@ -0,0 +1,22 @@
+//assertEqual function
+const assertEqual = function(actual, expected) {
+  //if two inputs don't match
+  if (actual !== expected) {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+    return;
+  }
+
+  //if two inputs match
+  console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+
+};
+
+// Head function
+const head = function(array) {
+  return array[0];
+};
+
+//Test the head function:
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), ); 
