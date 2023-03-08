c927a7ff6565bb9d407f818ab04bc925461351e2
Tara MacKinnon <taramackinnon4@gmail.com>
2023-03-08 03:42:40 +0000

    created function to compare arrays then log if the assertion passed or failed

---
+++ assertArraysEqual.js
@@ -0,0 +1,31 @@
+// compare the two arrays, if their length doesn't match, return false
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  // loop through the elements of the arrays, if they don't match return false
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  //otherwise return true
+  return true; 
+}
+
+//function will compare two arrays (using eqArrays function), will log if the assertion passed or failed
+const assertArraysEqual = function(array1, array2) {
+  
+  //if two arrays don't match, assertion failed
+  if (!eqArrays(array1, array2)) {
+    console.log(`❌ Assertion Failed`);
+    return;
+  }
+
+  //otherwise the arrays match, assertion passed
+  console.log(`✅ Assertion Passed`);
+
+};
+
+assertArraysEqual([1,2,3],[1,2,3]); // return pass
+assertArraysEqual([1,2,3],[1,4,3]); //return fail
