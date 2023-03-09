b4c8e23b5929e8178bcbf9a78e44154050a33be7
Tara MacKinnon <taramackinnon4@gmail.com>
2023-03-09 20:15:08 +0000

    fixed formatting with eslint

--- eqObjects.js
+++ eqObjects.js
@@ -13,29 +13,29 @@ const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
   }
-  // loop through the elements in each array 
+  // loop through the elements in each array
   for (let i = 0; i < array1.length; i++) {
     //if the element doesn't match then stop the loop and return false
     if (array1[i] !== array2[i]) {
       return false;
     }
   }
-  return true; 
-}
+  return true;
+};
 
 // FUNCTION eqObjects
 const eqObjects = function(object1, object2) {
-  let key1 = Object.keys(object1) //array of keys in object1
-  let key2 = Object.keys(object2)
-  //compare key length 
+  let key1 = Object.keys(object1); //array of keys in object1
+  let key2 = Object.keys(object2);
+  //compare key length
   if (key1.length !== key2.length) {
     return false;
   }
   // loop through the keys
-  for (key of key1) { 
+  for (let key of key1) {
     const value1 = object1[key];
     const value2 = object2[key];
-    
+
     // check if value is an array (doesn't matter which one)
     if (Array.isArray(value1)) {
       // if the arrays dont match return false
@@ -43,14 +43,14 @@ const eqObjects = function(object1, object2) {
         return false;
       }
       // use continue to start the loop of the next key
-      continue
-    } 
+      continue;
+    }
 
     //for values that aren't arrays, check if the values are equal
     // if the values aren't equal return false
-    if (value1 !== value2) {  
-    return false;
-    } 
+    if (value1 !== value2) {
+      return false;
+    }
   }
   //if it's not false for any of the cases above then it must be true!
   return true;
@@ -60,11 +60,11 @@ const eqObjects = function(object1, object2) {
 // //Test 1 (should pass):
 // const shirtObject = { color: "red", size: "medium" };
 // const anotherShirtObject= { size: "medium", color: "red" };
-// assertEqual((eqObjects(shirtObject , anotherShirtObject)), true); 
+// assertEqual((eqObjects(shirtObject , anotherShirtObject)), true);
 
 // //Test 2
 // const longSleeveShirtObject= { size: "medium", color: "red", sleeveLength: "long" };
-// assertEqual((eqObjects(shirtObject , longSleeveShirtObject)), false); 
+// assertEqual((eqObjects(shirtObject , longSleeveShirtObject)), false);
 
 
 // // Test 3
@@ -79,4 +79,4 @@ const eqObjects = function(object1, object2) {
 // //Test 5
 // const shirt1 = { colors: ["red", "blue"], size: ["medium", "small"] };
 // const shirt2 = { size: ["medium", "small"], colors: ["red", "blue"] };
-// assertEqual(eqObjects(shirt1, shirt2), true); 
+// assertEqual(eqObjects(shirt1, shirt2), true);
