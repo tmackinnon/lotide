52dd97125fd5576af559d1091f07984d837f06d0
Tara MacKinnon <taramackinnon4@gmail.com>
2023-03-09 20:24:54 +0000

    created assertObjectEqual function to assert whether objects are exactly the same

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,20 +1,18 @@
-// Function eqArrays
+//FUNCTION eqArrays
 const eqArrays = function(array1, array2) {
   //check that the lengths of the arrays match, otherwise return false
   if (array1.length !== array2.length) {
     return false;
   }
-
-  //// loop through the elements in each array 
+  // loop through the elements in each array
   for (let i = 0; i < array1.length; i++) {
     //if the element doesn't match then stop the loop and return false
     if (array1[i] !== array2[i]) {
       return false;
     }
   }
-  //otherwise return true
-  return true; 
-}
+  return true;
+};
 
 // FUNCTION eqObjects
 const eqObjects = function(object1, object2) {
@@ -26,12 +24,17 @@ const eqObjects = function(object1, object2) {
   }
 
   for (let key of key1) {
-    if (object1[key] !== object2[key]) {
-      if (Array.isArray(object1[key])) {
-        if (!eqArrays(object1[key], object2[key])) {
-          return false;
-        }
+    const value1 = object1[key];
+    const value2 = object2[key];
+
+    if (Array.isArray(value1)) {
+      if (!eqArrays(value1, value2)) {
+        return false;
       }
+      continue;
+    }
+
+    if (value1 !== value2) {
       return false;
     }
   }
@@ -41,7 +44,6 @@ const eqObjects = function(object1, object2) {
 // FUNCTION IMPLEMENTATION
 const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect;
-  console.log(`Example label: ${inspect(actual)}`);
   //if two objects don't match, assertion failed
   if (!eqObjects(actual, expected)) {
     console.log(`❌ Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
@@ -52,10 +54,7 @@ const assertObjectsEqual = function(actual, expected) {
   console.log(`✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
 };
 
-
-const shirt1 = { colors: ["red", "blue"], size: ["medium", "small"] };
-const shirt2 = { size: ["medium", "small"], colors: ["red", "blue"] };
-assertObjectsEqual(shirt1, shirt2);
-
-//NOTE:
-//console.log(`${inspect(actual)}`) returns: { colors: [ 'red', 'blue' ], size: [ 'medium', 'small' ] }
+// Test:
+// const shirt1 = { colors: ["red", "blue"], size: ["medium", "small"] };
+// const shirt2 = { size: ["medium", "small"], colors: ["red", "blue"] };
+// assertObjectsEqual(shirt1, shirt2);
