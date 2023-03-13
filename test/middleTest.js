f39cc2780fb8c70da8bf98d5ddf4a69facddd232
Tara MacKinnon <taramackinnon4@gmail.com>
2023-03-13 23:49:29 +0000

    implemented mocha/chai to test file

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,12 +1,24 @@
 const middle = require('../middle');
-const assertArraysEqual = require ('../assertArraysEqual');
+const assert = require('chai').assert;
 
-//Test 1:
-assertArraysEqual(middle([1, '3', '3']), ['3']) 
-assertArraysEqual(middle([]), []); 
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8]), [4, 5]);
+describe("#middle", () => {
+  
+  it("returns ['3'] as the middle value for [1, '3', '3']", () => {
+    assert.deepEqual(middle([1, '3', '3']), ['3']);
+  });
+  
+  it("returns [] as the middle value for ['1']", () => {
+    assert.deepEqual(middle(['1']), []);
+  });
 
-//Test 2:
-// Make sure the original array was not altered by the without function
-const words = ["hello", "world", "lighthouse"];
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+  it("returns [4, 5] as the middle value for [1, 2, 3, 4, 5, 6, 7, 8]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7, 8]), [4, 5]);
+  });
+
+  it("ensures original array was not altered when function is run", () => {
+    const words = ["hello", "world", "lighthouse"];
+    middle(words);
+    assert.deepEqual(words, ["hello", "world", "lighthouse"]);
+  });
+
+});
