7fc95df45ac4d6238aa56d95e5b169efcae55b86
Tara MacKinnon <taramackinnon4@gmail.com>
2023-03-13 20:03:54 +0000

    removed a console.log from test file

--- test/middleTest.js
+++ test/middleTest.js
@@ -9,5 +9,4 @@ assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8]), [4, 5]);
 //Test 2:
 // Make sure the original array was not altered by the without function
 const words = ["hello", "world", "lighthouse"];
-console.log(middle(words));
 assertArraysEqual(words, ["hello", "world", "lighthouse"]);
