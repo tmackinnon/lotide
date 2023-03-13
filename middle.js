8c89c64ef84edbe0a64827f1f2c81a3f4ae7b0a7
Tara MacKinnon <taramackinnon4@gmail.com>
2023-03-13 19:46:53 +0000

    refactored middle function and added test file

--- middle.js
+++ middle.js
@@ -1,32 +1,3 @@
-// eqArrays function
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  // loop through the elements of the arrays, if they don't match return false
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  //otherwise return true
-  return true; 
-}
-
-// asserArraysEqual function
-const assertArraysEqual = function(array1, array2) {
-  
-  //if two arrays don't match, assertion failed
-  if (!eqArrays(array1, array2)) {
-    console.log(`❌ Assertion Failed`);
-    return;
-  }
-
-  //otherwise the arrays match, assertion passed
-  console.log(`✅ Assertion Passed`);
-
-};
-
 // middle function
 const middle = function(array) {
   let output = [];
@@ -47,22 +18,4 @@ const middle = function(array) {
   return output;
 }
 
-//Test 1:
-console.log(middle([1, 2, 3, 4, 5, 6, 7, 8])) // output:[4, 5]
-console.log(middle([1, '3', '3'])) // output:['3']
-console.log(middle([])) // output:[]
-
-//Test 2:
-// Make sure the original array was not altered by the without function
-const words = ["hello", "world", "lighthouse"];
-console.log(middle(words));
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
-
-/* PLAN
-input: takes in an array 
-processing: 3 possibilities
-  1 - Arrays with one or two elements, there is no middle. Return an empty array.
-  2 - Arays with odd number of elements, return array containing a single middle element
-  3 - Arrays with even number of elements, return an array containing the two elements in the middle 
-output: an array (either empty, with one element or with 2 elements )
-*/
+module.exports = middle;
---
+++ test/middleTest.js
@@ -0,0 +1,13 @@
+const middle = require('../middle');
+const assertArraysEqual = require ('../assertArraysEqual');
+
+//Test 1:
+assertArraysEqual(middle([1, '3', '3']), ['3']) 
+assertArraysEqual(middle([]), []); 
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8]), [4, 5]);
+
+//Test 2:
+// Make sure the original array was not altered by the without function
+const words = ["hello", "world", "lighthouse"];
+console.log(middle(words));
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
